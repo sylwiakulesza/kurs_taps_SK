d54822573f38827bd21939c50ed0c6dfaadd4f8d
Sylwia <sylwiakulesza13@gmail.com>
2020-10-29 22:06:58 +0100

    kalkulator

---
+++ scripts/operatory.py
@@ -0,0 +1,49 @@
+
+number = 1
+number1 = 2
+print(number + number1)
+
+list = ['Ala', 'ma', 'kota']
+print(list[0])
+print(list[0:3])
+
+text = 'test'
+text1 = 'czesc, jestem "Sylwia"'
+print(text1)
+
+cities = {'city': ['Warszawa', 'Gdańsk', 'Bratysława']}
+
+print(cities['city'])
+print(cities['city'][2])
+
+suma = 1 + 2
+print(suma)
+
+roznica = 1 - 2
+print(roznica)
+
+reszta = 11 % 3
+print(reszta)
+
+kwadrat = 10 ** 2
+print(kwadrat)
+
+szescian = 10 ** 3
+print(szescian)
+
+print('zmienna ' + 'jest ' + 'super')
+
+zmienna = 'no siema ' * 10
+print(zmienna)
+
+parzyste = [2, 4, 6, 8]
+nieparzyste = [1, 3, 5, 7]
+
+naturalne = parzyste + nieparzyste
+print(naturalne)
+
+print(1 > 1)
+print(1 < 1)
+print(1 == 2)
+print(1 != 2)
+
---
+++ scripts/skrypt.py
@@ -0,0 +1,4 @@
+import random
+
+liczba = random.randint(1,10)
+print ('wylosowana liczba to: ', liczba)
